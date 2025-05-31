36103d05eac30d118c7191cc070dbf3b96f3a237
Hyun Heechan <hyeonhc328@gmail.com>
2025-05-31 15:46:45 +0900

    feat(OutPutView): main()에서 출력 책임 분리, OutputView가 담당하도록 함

--- src/CarMain.java
+++ src/CarMain.java
@@ -1,17 +1,8 @@
 import constansts.Destination;
 import view.InputView;
+import view.OutputView;
 
 public class CarMain {
-    public static final String LINE_1_START = "=======";
-    public static final String LINE_1_END = "=======";
-    public static final String LINE_2_START = "총 비용 : ";
-    public static final String LINE_2_END = "원";
-    public static final String LINE_3_START = "총 주유 횟수 : ";
-    public static final String LINE_3_END = "회";
-    public static final String LINE_4_START = "총 이동 시간 : ";
-    public static final String LINE_4_HOUR = "시간 ";
-    public static final String LINE_4_MINUTE = "분";
-
     final static String INPUT_DESTINATION = "이동 지역 선택 [1]부산 [2]대전 [3]강릉 [4]광주 : ";
     final static String INPUT_PASSENGERS = "이동할 승객 수 입력 : ";
     final static String INPUT_CAR_TYPE = "이동할 차량 선택 [1]스포츠카 [2]승용차 [3]버스 : ";
@@ -19,13 +10,13 @@ public class CarMain {
     final static String INPUT_WEATHER_CONDITION = "날씨 [1]맑음 [2]비 [3]눈 : ";
     public static void main(String[] args) {
         InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
         int destination = Destination.getDistanceById(inputView.getInt(INPUT_DESTINATION));
         int passengers = inputView.getInt(INPUT_PASSENGERS);
         int carType = inputView.getInt(INPUT_CAR_TYPE);
         boolean isModeOn = inputView.getBoolean(INPUT_MODE);
         int weatherCondition = inputView.getInt(INPUT_WEATHER_CONDITION);
-
-        System.out.println();
 
         Car car = selectCarImpl(carType, isModeOn);
 
@@ -35,17 +26,11 @@ public class CarMain {
         int totalRefuel = car.countRefuel(totalFuel);
         int totalCost = car.totalCost(totalFuel);
         int totalTravelTime = car.totalTravelTime(destination, totalMovement, weatherCondition);
-        printResult(car.name, totalCost, totalRefuel, totalTravelTime);
+
+        outputView.printResult(car.name, totalCost, totalRefuel, totalTravelTime);
     }
 
-    public static void printResult(String carName,int totalCost, int totalRefuel, int totalTravelTime) {
-        System.out.println("\n"+LINE_1_START + carName + LINE_1_END);
-        System.out.println(LINE_2_START + totalCost + LINE_2_END);
-        System.out.println(LINE_3_START + totalRefuel + LINE_3_END);
-        System.out.println(LINE_4_START + (int) ((double) totalTravelTime / 60) + LINE_4_HOUR +
-                totalTravelTime % 60 + LINE_4_MINUTE);
-        System.out.println(LINE_1_START + " ======= " + LINE_1_END);
-    }
+
 
     private static Car selectCarImpl(int carType, boolean isModeOn) {
         Car car;
--- src/view/OutputView.java
+++ src/view/OutputView.java
@@ -10,13 +10,14 @@ public final String LINE_3_END = "회";
 public final String LINE_4_START = "총 이동 시간 : ";
 public final String LINE_4_HOUR = "시간 ";
 public final String LINE_4_MINUTE = "분";
-    public void printResult(String carName,int totalCost, int totalRefuel, double totalTravelTime) {
-
+    public void printResult(String carName,int totalCost, int totalRefuel, int totalTravelTime) {
+        System.out.println();
         System.out.println(LINE_1_START + carName + LINE_1_END);
         System.out.println(LINE_2_START + totalCost + LINE_2_END);
         System.out.println(LINE_3_START + totalRefuel + LINE_3_END);
-        System.out.println(LINE_4_START + (int)totalTravelTime / 60 + LINE_4_HOUR +
-                (int)totalTravelTime % 60 + LINE_4_MINUTE);
+        System.out.println(LINE_4_START +
+                totalTravelTime / 60 + LINE_4_HOUR +
+                totalTravelTime % 60 + LINE_4_MINUTE);
         System.out.println(LINE_1_START + "=======" + LINE_1_END);
     }
 
